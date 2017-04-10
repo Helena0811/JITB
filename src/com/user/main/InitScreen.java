8ecad84f6dc27fa182400b328c05b66cd7949900
MSDN-SPECIAL\Administrator <yeonsil083@gmail.com>
2017-04-10 19:55:00 +0900

    사용자 연실쓰

--- src/com/user/main/InitScreen.java
+++ src/com/user/main/InitScreen.java
@@ -1,29 +1,94 @@
+/*
+ * 화면을 터치하세요 첫번째 창
+ */
 package com.user.main;
 
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import com.user.frame.ScreenFrame;
 
-public class InitScreen extends ScreenFrame implements ActionListener{
+public class InitScreen extends ScreenFrame implements ActionListener {
+
 	private JButton next;
-	
+	Image img;
+
+	Canvas can;
+	Toolkit kit;
+
+	JPanel panel;
+	JButton bt;
+
 	public InitScreen(Main main) {
 		super(main);
-		
+
 		next = new JButton("다음 페이지");
 		next.addActionListener(this);
-		
+
 		add(next);
+
+		can = new Canvas();
+		panel = new JPanel();
+		bt = new JButton("화면을 터치하세요");
+		// bt_poster=new JButton(new ImageIcon("C:/project
+		// workspace/JITB/res/pt.jpg"));
+
+		// panel.add(bt_poster,BorderLayout.CENTER);
+		panel.add(bt, BorderLayout.CENTER);
+		add(bt);
+		add(can);
+
+		URL url = getClass().getResource("/pt.jpg");
+
+		try {
+			img = ImageIO.read(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		can = new Canvas(){
+		      
+			@Override
+		      public void paint(Graphics g) {
+		         g.drawImage(img, 0, 0, 50, 100, this);
+			
+		      }
+		   };
+		can.setPreferredSize(new Dimension(50,50));
+		JFrame frame = new JFrame();
+		frame.setSize(300, 300);
+		JLabel label = new JLabel(new ImageIcon(img));
+		frame.add(label);
+		frame.setVisible(true);
+
+		bt.setFocusPainted(false);
+		bt.setBorderPainted(false);
+		bt.setContentAreaFilled(false);
+
+		setSize(450, 700);
+		setVisible(true);
+
 	}
-	
-	@Override
+
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		
-		if(obj == next){
+
+		if (obj == next) {
 			main.setPage(1);
 		}
 	}
--- src/com/user/main/purchase/MenuChoiceScreen.java
+++ src/com/user/main/purchase/MenuChoiceScreen.java
@@ -1,31 +1,72 @@
 package com.user.main.purchase;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import com.user.frame.ScreenFrame;
 import com.user.main.Main;
 
-public class MenuChoiceScreen extends ScreenFrame implements ActionListener{
-	private JButton next;
-	
+public class MenuChoiceScreen extends ScreenFrame implements ActionListener {
+
+	JButton next;
+	JPanel panel;
+	// JTextArea area;
+	JButton bt_ticketprint, bt_ticket, bt_combo;
+
 	public MenuChoiceScreen(Main main) {
 		super(main);
-		
+
 		next = new JButton("다음 페이지");
 		next.addActionListener(this);
-		
+
 		add(next);
+
+		panel = new JPanel();
+		// area = new JTextArea();
+		bt_ticketprint = new JButton("예매티켓출력");
+		bt_ticket = new JButton("예매티겟 구매");
+		bt_combo = new JButton("콤보");
+
+		bt_ticket.addActionListener(this);
+
+		panel.setLayout(new FlowLayout());
+
+		// panel.add(area);
+		panel.add(bt_ticketprint);
+		panel.add(bt_ticket);
+		panel.add(bt_combo);
+
+		// add(area);
+		add(panel);
+
+		// bt_ticketprint.setFocusPainted(false);
+		// bt_ticket.setBorderPainted(false);
+		// setContentAreaFilled(false);
+
+		setBackground(new Color(21,21,21));
+		bt_ticketprint.setSize(300, 400);
+		bt_ticket.setSize(300, 400);
+		bt_combo.setSize(300, 400);
+
+		setSize(400, 600);
+		setVisible(true);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
-		
-		if(obj == next){
+
+		if (obj == next) {
 			main.setPage(2);
+		} else if (obj == bt_ticket) {
+			JOptionPane.showMessageDialog(this, "출력메시지");
 		}
 	}
 }
